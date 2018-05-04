fc6a6f271321c534d5c3c8a1c3404ae1b40ac29f
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-04 11:54:06 +0300

    added a django project and django app

---
+++ pinapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http  import HttpResponse
+
+
+def welcome(request):
+    return HttpResponse('Welcome to the Moringa Tribune')
+
+# Create your views here.
