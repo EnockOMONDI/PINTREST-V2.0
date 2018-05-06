7eef54770e89766a7ea606328e12f7e740901fb9
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-06 05:37:04 +0300

    installed bootstrap and included it in my settings installed apps

--- pintrest/settings.py
+++ pintrest/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
 
     'pinapp',
-    'bootstrap3',
+    'bootstrapsd3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
