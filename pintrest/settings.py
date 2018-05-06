4cea17358d632f6850a5bf5200670538987d507c
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-07 01:59:13 +0300

    added  Postgres configurations.

--- pintrest/settings.py
+++ pintrest/settings.py
@@ -78,10 +78,13 @@ WSGI_APPLICATION = 'pintrest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tribune',
+        'USER': 'sean',
+    'PASSWORD':'Aggreyomondi90',
     }
 }
+
 
 
 # Password validation
