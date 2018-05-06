6c185914c52d12a39877560297c41d09adc30b73
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-06 05:36:04 +0300

    installed bootstrap and included it in my settings installed apps

--- pinapp/views.py
+++ pinapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 
 #views
+'''
 def welcome(request):
     return HttpResponse('Pintrest')
+    '''
 
 #pintresthomepageview
 def welcome(request):
--- pintrest/settings.py
+++ pintrest/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pinapp.apps.PinappConfig',
+
     'pinapp',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
