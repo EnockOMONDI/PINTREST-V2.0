536006b2ad1772ce6167df1a6876d4f63f4f09a7
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-06 03:27:16 +0300

    added notes

--- pinapp/urls.py
+++ pinapp/urls.py
@@ -1,5 +1,9 @@
+'''To design URLs for an app, you create a Python module informally called a URLconf (URL configuration).
+ This module is pure Python code and is a mapping between URL path expressions to Python functions (your views).
+ '''
 from django.conf.urls import url
 from . import views
+
 #url responsible for maping welcome function
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
