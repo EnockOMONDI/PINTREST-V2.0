7742ce358bf2ddd8f18c78e34bb43572ae430a7a
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-07 10:53:17 +0300

    installed bootstrap 4 and used cards

--- pintrest/settings.py
+++ pintrest/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
 
     'pinapp',
-    'bootstrap3',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
